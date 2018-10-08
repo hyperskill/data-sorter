55decc9a9b17f6a1e9810a548c19227d8745f3af
Artyom Burylov <swsms@mail.ru>
2018-10-08 18:22:46 +0500

    Update and rename Main.java to SortingTool.java

---
+++ src/sorter/SortingTool.java
@@ -0,0 +1,14 @@
+package sorter;
+
+import java.util.*;
+
+public class SortingTool {
+    public static void main(final String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while (scanner.hasNextLong()) {
+            long number = scanner.nextLong();
+            // write your code here
+        }
+    }
+}
