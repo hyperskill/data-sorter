ec056704a354a1f191739eb29a1b97787d1b912b
MercuryZ <15210820708@163.com>
2018-11-05 23:04:09 +0800

    SortingTool.java

--- src/sorter/SortingTool.java
+++ src/sorter/SortingTool.java
@@ -1,14 +1,24 @@
 package sorter;
 
-import java.util.*;
+import java.util.Scanner;
 
 public class SortingTool {
     public static void main(final String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        int total = 0;
+        long greatest = 0;
+        int times = 1;
         while (scanner.hasNextLong()) {
-            long number = scanner.nextLong();
-            // write your code here
+            final long number = scanner.nextLong();
+            total++;
+            if (number>greatest) {
+                greatest = number;
+            }
+            else if(number==greatest) {
+                times++;
+            }
         }
+        System.out.println("Total numbers: " + total);
+        System.out.println("The greatest number: " + greatest + "( " + times + " time(s))");
     }
 }
